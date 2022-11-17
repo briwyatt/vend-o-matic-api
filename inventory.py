8990cdf25afa395beb3a94e82d1231e3f48cb2a1
Bri Wyatt <bri.wyatt@smiledirectclub.com>
2022-11-17 10:00:57 -0600

    cleaned up the codebase

--- inventory.py
+++ inventory.py
@@ -5,21 +5,28 @@ def get_timestamp():
 
 INVENTORY = {
     "Pepsi": {
+        "id": 1,
         "name": "Pepsi",
         "quantity": 5,
         "timestamp": get_timestamp(),
     },
     "Dr. Pepper": {
+        "id": 2,
         "name": "Dr. Pepper",
         "quantity": 5,
         "timestamp": get_timestamp(),
     },
     "Mountain Dew": {
+        "id": 3,
         "name": "Mountain Dew",
         "quantity": 5,
         "timestamp": get_timestamp(),
     }
 }
 
+
+
 def read_all():
     return list(INVENTORY.values())
+
+
