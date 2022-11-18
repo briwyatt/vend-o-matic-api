3616029c6acd9c790ae667d1159a369b3616557c
Bri Wyatt <bri.wyatt@smiledirectclub.com>
2022-11-18 10:38:44 -0600

    get rid of unnecessary properties

--- inventory.py
+++ inventory.py
@@ -1,23 +1,15 @@
-from datetime import datetime
-
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
 INVENTORY = {
     "Pepsi": {
         "name": "Pepsi",
         "quantity": 5,
-        "timestamp": get_timestamp(),
     },
     "Dr. Pepper": {
         "name": "Dr. Pepper",
         "quantity": 5,
-        "timestamp": get_timestamp(),
     },
     "Mountain Dew": {
         "name": "Mountain Dew",
         "quantity": 5,
-        "timestamp": get_timestamp(),
     }
 }
 
