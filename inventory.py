673da3ce2081716ccc09bd637ff24166178902c1
Bri Wyatt <bri.wyatt@smiledirectclub.com>
2022-11-18 11:38:33 -0600

    add more paths

--- inventory.py
+++ inventory.py
@@ -1,3 +1,5 @@
+from flask import abort
+
 INVENTORY = {
     "Pepsi": {
         "name": "Pepsi",
@@ -16,3 +18,17 @@ INVENTORY = {
 def read_all():
     inventory_list = list(INVENTORY.values())
     return [d['quantity'] for d in inventory_list if 'quantity' in d]
+
+def create(item):
+    name = item.get("name")
+    quantity = item.get("quantity")
+    if name and quantity not in INVENTORY:
+        INVENTORY[name] = {
+            "name": name,
+        }
+        return INVENTORY[name], 201
+    else:
+        abort(
+            406,
+            f"Inventory item with name {name} already exists",
+        )
